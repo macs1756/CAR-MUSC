52ea00e7cc3af09b356474eaf2e403c031a3e5fe
macs1756 <kindrativmaksum312@gmail.com>
2022-11-18 21:56:12 +0300

    add base

---
+++ js/app.js
@@ -0,0 +1,69 @@
+
+
+
+
+function isWebp(){
+	function testWebp(callback) {
+		let webP = new Image();
+		webP.onload = webP.onerror = function (){
+			callback(webP.height == 2);
+		};
+		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+
+	}
+	testWebp(function (support){
+		let className = support = true ? "webp" : "no-webp";
+		document.documentElement.classList.add(className);
+	})
+}
+	
+isWebp();
+
+
+
+const btnBurger = document.querySelector(".header__burger");
+const burger = document.querySelector(".burger__body");
+
+
+
+
+
+
+
+btnBurger.addEventListener("click", item =>{
+		btnBurger.classList.toggle("close");
+});
+
+
+
+btnBurger.addEventListener("click", ()=>{
+		document.querySelector("body").classList.toggle("scrollNone");
+		burger.classList.toggle("active");
+		btnBurger.classList.remove("header__burger_close");
+});
+
+
+
+
+
+
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", ()=>{
+	if(scrollY>10){
+		header.classList.add("active");
+	}else{
+		header.classList.remove("active");
+	}
+})
+
+
+
+
+
+
+
+
+
+

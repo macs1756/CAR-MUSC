86eac7a51bc9768458ff9fd254a89686bfc4cf13
macs1756 <kindrativmaksum312@gmail.com>
2022-11-19 19:23:57 +0300

    add animation diamont the section initial

--- js/app.js
+++ js/app.js
@@ -1,122 +1,126 @@
-
-
-
-
-function isWebp(){
-	function testWebp(callback) {
-		let webP = new Image();
-		webP.onload = webP.onerror = function (){
-			callback(webP.height == 2);
-		};
-		webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-
-	}
-	testWebp(function (support){
-		let className = support = true ? "webp" : "no-webp";
-		document.documentElement.classList.add(className);
-	})
+function isWebp() {
+  function testWebp(callback) {
+    let webP = new Image();
+    webP.onload = webP.onerror = function () {
+      callback(webP.height == 2);
+    };
+    webP.src =
+      "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+  }
+  testWebp(function (support) {
+    let className = (support = true ? "webp" : "no-webp");
+    document.documentElement.classList.add(className);
+  });
 }
-	
+
 isWebp();
-
-
 
 const btnBurger = document.querySelector(".header__burger");
 const burger = document.querySelector(".burger__body");
 
-
-
-
-
-
-
-btnBurger.addEventListener("click", item =>{
-		btnBurger.classList.toggle("close");
+btnBurger.addEventListener("click", (item) => {
+  btnBurger.classList.toggle("close");
 });
 
-
-
-btnBurger.addEventListener("click", ()=>{
-		burger.classList.toggle("burger__body-active");
-		btnBurger.classList.remove("header__burger_close");
+btnBurger.addEventListener("click", () => {
+  burger.classList.toggle("burger__body-active");
+  btnBurger.classList.remove("header__burger_close");
 });
-
-
-
-
-
-
 
 let header = document.querySelector("header");
 
-window.addEventListener("scroll", ()=>{
-	if(scrollY>10){
-		header.classList.add("header-active");
-	}else{
-		header.classList.remove("header-active");
-	}
-})
-
-
-
-
-let diamondParent = document.querySelectorAll(".initial__dekor");
-
-
-
-
+window.addEventListener("scroll", () => {
+  if (scrollY > 10) {
+    header.classList.add("header-active");
+  } else {
+    header.classList.remove("header-active");
+  }
+});
 
 var ml4 = {};
-ml4.opacityIn = [0,1];
+ml4.opacityIn = [0, 1];
 ml4.scaleIn = [0.2, 1];
 ml4.scaleOut = 3;
 ml4.durationIn = 800;
 ml4.durationOut = 600;
 ml4.delay = 500;
 
-anime.timeline({loop: true})
+anime
+  .timeline({ loop: true })
   .add({
-    targets: '.ml4 .letters-1',
+    targets: ".ml4 .letters-1",
     opacity: ml4.opacityIn,
     scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-1',
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-1",
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-2',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
     opacity: ml4.opacityIn,
     scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-2',
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-2",
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-3',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
     opacity: ml4.opacityIn,
     scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-3',
+    duration: ml4.durationIn,
+  })
+  .add({
+    targets: ".ml4 .letters-3",
     opacity: 0,
     scale: ml4.scaleOut,
     duration: ml4.durationOut,
     easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4',
+    delay: ml4.delay,
+  })
+  .add({
+    targets: ".ml4",
     opacity: 0,
     duration: 500,
-    delay: 500
+    delay: 500,
   });
+
+
+
+
+
+let diamondParent = document.querySelectorAll(".dekor");
+
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
+ }
+
+
+ setInterval(()=>{
+ 	if (diamondParent) {
+ 		diamondParent.forEach((item) => {
+		  
+ 			 item.classList.add("active");
+			 	 
+			 delay(1990).then(() =>{
+				item.classList.remove("active");
+			 });		
+ 	 })
+   }
+
+ }, 2100);
+
 
 
 
